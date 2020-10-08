c3d94881896e89728205dbee5de368d266393b4a
John Paul Ong <jpong5202@gmail.com>
2020-10-08 10:07:15 +0800

    Updated Endpoints

--- routes/api/items.js
+++ routes/api/items.js
@@ -30,15 +30,16 @@ const GET_ONE = {
   inventory: {},
 };
 
-router.get("/getAll/:type", (req, res) => {
+router.get("/:type", (req, res) => {
   const { client, inventory } = GET_ALL;
   switch (req.params.type) {
     case "client":
       return Item.find({ is_hidden: false }, client, (err, items) => {
         if (err)
-          return res
-            .status(400)
-            .json({ err, msg: "Error occurred while fetching items data." });
+          return res.json({
+            err,
+            msg: "Error occurred while fetching items data.",
+          });
         return items.length === 0
           ? res
               .status(200)
@@ -49,9 +50,10 @@ router.get("/getAll/:type", (req, res) => {
     case "inventory":
       return Item.find({}, inventory, (err, items) => {
         if (err)
-          return res
-            .status(400)
-            .json({ err, msg: "Error occurred while fetching items data." });
+          return res.json({
+            err,
+            msg: "Error occurred while fetching items data.",
+          });
         return items.length === 0
           ? res
               .status(200)
@@ -62,9 +64,10 @@ router.get("/getAll/:type", (req, res) => {
     case "super":
       return Item.find({}, (err, items) => {
         if (err)
-          return res
-            .status(400)
-            .json({ err, msg: "Error occurred while fetching items data." });
+          return res.json({
+            err,
+            msg: "Error occurred while fetching items data.",
+          });
         return items.length === 0
           ? res
               .status(200)
@@ -73,11 +76,11 @@ router.get("/getAll/:type", (req, res) => {
       });
 
     default:
-      return res.status(400).json({ msg: "Invalid system type parameter." });
+      return res.json({ msg: "Invalid system type parameter." });
   }
 });
 
-router.get("/getOne/:type", (req, res) => {
+router.get("/item/:type", (req, res) => {
   const { client, inventory } = GET_ONE;
   switch (req.params.type) {
     case "client":
@@ -92,91 +95,83 @@ router.get("/getOne/:type", (req, res) => {
             });
           return item
             ? res.status(200).json({ item })
-            : res.status(404).json({ msg: "Item does not exist." });
+            : res.json({ msg: "Item does not exist." });
         }
       );
     case "inventory":
       return Item.findById(req.query.id, inventory, (err, item) => {
         if (err)
-          return res.status(400).json({
+          return res.json({
             err,
             msg: "Error occurred while fetching item data.",
           });
         return item
           ? res.status(200).json({ item })
-          : res.status(404).json({ msg: "Item does not exist." });
+          : res.json({ msg: "Item does not exist." });
       });
     default:
-      return res.status(400).json({ msg: "Invalid system type parameter." });
+      return res.json({ msg: "Invalid system type parameter." });
   }
 });
 
-router.post("/postOne", (req, res) => {
+router.post("/post", (req, res) => {
   const newItem = new Item(req.body);
   newItem.save({}, (err, item) => {
     if (err)
-      return res
-        .status(400)
-        .json({ err, msg: "Error occurred while posting item." });
+      return res.json({ err, msg: "Error occurred while posting item." });
     return res.status(200).json({ item, msg: "Item posted successfully." });
   });
 });
 
-router.post("/editOne", (req, res) => {
+router.post("/edit", (req, res) => {
   Item.findByIdAndUpdate(req.query.id, req.body, { new: true }, (err, item) => {
     if (err)
-      return res
-        .status(400)
-        .json({ err, msg: "Error occurred while editing item data." });
+      return res.json({ err, msg: "Error occurred while editing item data." });
     return item
       ? res.status(200).json({ item, msg: "Changes successfully saved." })
-      : res.status(404).json({ msg: "Item does not exist." });
+      : res.json({ msg: "Item does not exist." });
   });
 });
 
-router.post("/disableOne", (req, res) => {
+router.post("/disable", (req, res) => {
   Item.findByIdAndUpdate(
     req.query.id,
     { is_hidden: true },
     { new: true },
     (err, item) => {
-      if (err)
-        return res.status(400).json({ err, msg: "Something went wrong." });
+      if (err) return res.json({ err, msg: "Something went wrong." });
       return item
         ? res
             .status(200)
             .json({ item, msg: "Item successfully disabled temporarily." })
-        : res.status(404).json({ msg: "Item does not exist." });
+        : res.json({ msg: "Item does not exist." });
     }
   );
 });
 
-router.post("/enableOne", (req, res) => {
+router.post("/enable", (req, res) => {
   Item.findByIdAndUpdate(
     req.query.id,
     { is_hidden: false },
     { new: true },
     (err, item) => {
-      if (err)
-        return res.status(400).json({ err, msg: "Something went wrong." });
+      if (err) return res.json({ err, msg: "Something went wrong." });
       return item
         ? res
             .status(200)
             .json({ item, msg: "Item successfully set to enabled." })
-        : res.status(404).json({ msg: "Item does not exist." });
+        : res.json({ msg: "Item does not exist." });
     }
   );
 });
 
-router.delete("/deleteOne", (req, res) => {
+router.delete("/delete", (req, res) => {
   Item.findByIdAndDelete(req.query.id, (err, item) => {
     if (err)
-      return res
-        .status(400)
-        .json({ err, msg: "Error occurred while deleting item." });
+      return res.json({ err, msg: "Error occurred while deleting item." });
     return item
       ? res.status(200).json({ item, msg: "Item permanently deleted." })
-      : res.status(404).json({ msg: "Item does not exist." });
+      : res.json({ msg: "Item does not exist." });
   });
 });
 
--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,29 +4,28 @@ const router = express.Router();
 const Order = require("../../models/Order");
 const Item = require("../../models/Item");
 
-router.get("/getAll", (req, res) => {
+router.get("/", (req, res) => {
   Order.find({}, (err, orders) => {
     if (err)
-      return res
-        .status(400)
-        .json({ err, msg: "Error occurred while fetching orders." });
+      return res.json({ err, msg: "Error occurred while fetching orders." });
     return res.status(200).json({ orders });
   });
 });
 
-router.get("/getOne", (req, res) => {
+router.get("/order", (req, res) => {
   Order.findById(req.query.id, (err, order) => {
     if (err)
-      return res
-        .status(400)
-        .json({ err, msg: "Error occurred while fetching order data." });
+      return res.json({
+        err,
+        msg: "Error occurred while fetching order data.",
+      });
     return order
       ? res.status(200).json({ order })
-      : res.status(404).json({ msg: "Order does not exist." });
+      : res.json({ msg: "Order does not exist." });
   });
 });
 
-router.post("/generateOrder", (req, res) => {
+router.post("/generate", (req, res) => {
   const { item_id } = req.body;
   const newOrder = new Order({ item: {}, user: {}, ...req.body });
   Item.findById(
@@ -34,28 +33,27 @@ router.post("/generateOrder", (req, res) => {
     { item_name: 1, item_price: 1, sku: 1 },
     (err, item) => {
       if (err)
-        return res
-          .status(400)
-          .json({ err, msg: "Error occurred while fetching item data." });
+        return res.json({
+          err,
+          msg: "Error occurred while fetching item data.",
+        });
       if (!item) return res.status(404).json({ msg: "Item does not exist." });
       newOrder.item = item;
       newOrder.save({}, (err, order) => {
         if (err)
-          return res
-            .status(400)
-            .json({ err, msg: "Error occurred while generating order." });
+          return res.json({
+            err,
+            msg: "Error occurred while generating order.",
+          });
         res.status(200).json({ order, msg: "Order successfully generated." });
       });
     }
   );
 });
 
-router.delete("/deleteOne", (req, res) => {
+router.delete("/delete", (req, res) => {
   Order.findByIdAndDelete(req.query.id, (err, order) => {
-    if (err)
-      return res
-        .status(400)
-        .json({ msg: "Error occurred while deleting order." });
+    if (err) return res.json({ msg: "Error occurred while deleting order." });
     return res.status(200).json({ order, msg: "Order permanently deleted." });
   });
 });
