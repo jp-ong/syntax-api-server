bd6ffbe065374ab2b6106fade72b93e88acaa8dc
John Paul Ong <jpong5202@gmail.com>
2020-10-20 21:37:13 +0800

    Forbidden - Denied

--- middleware/whitelist.js
+++ middleware/whitelist.js
@@ -2,7 +2,7 @@ const ips = process.env.WHITELIST.split("+");
 
 const whitelist = (req, res, next) => {
   if (!ips.includes(req.ip)) {
-    console.log(new Date().toLocaleString() + " >" + req.ip + " #FORBIDDEN");
+    console.log(new Date().toLocaleString() + " >" + req.ip + " #DENIED");
     return res.status(403).json({ error: 403, msg: "Forbidden" });
   }
   console.log(new Date().toLocaleString() + " >" + req.ip + " #SUCCESS");
