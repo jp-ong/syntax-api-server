ad149a8e4ca6052da4fb93fd08c77d94015e0278
John Paul Ong <jpong5202@gmail.com>
2020-10-20 22:53:15 +0800

    Added env variable

--- middleware/whitelist.js
+++ middleware/whitelist.js
@@ -1,7 +1,8 @@
 const ips = process.env.WHITELIST.split("+");
+const storeIP = process.env.STORE_IP;
 
 const whitelist = (req, res, next) => {
-  if (!ips.includes(req.ip)) {
+  if (!ips.includes(req.ip) && storeIP !== req.ip) {
     console.log(new Date().toLocaleString() + " >" + req.ip + " #DENIED");
     return res.status(403).json({ error: 403, msg: "Forbidden" });
   }
