c391541c0dd99ab3ad70c15ce880f45f68e8ab51
John Paul Ong <jpong5202@gmail.com>
2020-10-28 21:38:01 +0800

    Added whitelist switch

--- middleware/whitelist.js
+++ middleware/whitelist.js
@@ -4,7 +4,8 @@ const whitelist = (req, res, next) => {
   try {
     const ips = process.env.WHITELIST.split("+");
     const storeIP = process.env.STORE_IP;
-    if (!ips.includes(req.ip) && storeIP !== req.ip) {
+    const whitelistStatus = process.env.WHITELIST_STATUS;
+    if (!ips.includes(req.ip) && storeIP !== req.ip && whitelistStatus === 1) {
       console.error(`~~~~~@${date}~~~~~>${req.ip}~~~~~#DENIED~~~~~`);
       return res.status(403).json({ msg: "Forbidden", status: 403 });
     } else {
